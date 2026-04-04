9c2a50815c95bad36840aed86f6dbfd148b65076
simop0409 <s.pede@campus.tu-berlin.de>
2026-04-04 14:20:24 +0200

    feat: integrate Pinia for state management and add board store

--- frontend/src/components/Tag.vue
+++ frontend/src/components/Tag.vue
@@ -11,25 +11,25 @@
 
     const colorClass = computed(() => {
     switch (props.tagText) {
-        // Blau-Töne für Technik
+        //Blau-Töne für Technik
         case 'Backend':
         case 'Database':
         case 'Logic':
             return "bg-primary-subtle text-primary border border-primary-subtle";
         
-        // Türkis/Info für Frontend & UI
+        //Türkis/Info für Frontend & UI
         case 'Frontend':
         case 'UI':
         case 'Feature':
             return 'bg-info-subtle text-info border border-info-subtle';
         
-        // Lila/Violett für Design & UX
+        //Lila/Violett für Design & UX
         case 'Design':
         case 'UX':
         case 'Animation':
             return 'bg-secondary text-white'; 
 
-        // Warnfarben (falls du sie mal wieder einbaust)
+        //Warnfarben (falls du sie mal wieder einbaust)
         case 'Urgent':
         case 'Bug':
             return 'bg-danger';
